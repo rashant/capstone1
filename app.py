efafe79bb9abd9a2d7fe603e637506ae4a268538
rashant <saip96260@gmail.com>
2024-08-01 23:48:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import psycopg2
+import bcrypt
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+# Database connection
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname='demo',
+        user='postgres',
+        password='root',
+        host='localhost'
+    )
+    return conn
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        
+        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cur.fetchone()
+        
+        if user:
+            return "User already exists!"
+        
+        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        conn.commit()
+        
+        cur.close()
+        conn.close()
+        
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        conn = get_db_connection()
+        cur = conn.cursor()
+        
+        cur.execute("SELECT * FROM users WHERE username = %s and password = %s", (username, password))
+        user = cur.fetchone()
+        
+        cur.close()
+        conn.close()
+        
+        if user:
+            session['username'] = user[1]
+            return redirect(url_for('welcome'))
+        return "Invalid username or password!"
+    return render_template('login.html')
+
+@app.route('/welcome')
+def welcome():
+    if 'username' in session:
+        username = session['username']
+        return render_template('welcome.html', username=username)
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
